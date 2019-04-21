29a861eda98a0870c97466e3a904f09ac8a5f02d
Almar Klein <almar.klein@gmail.com>
2019-04-21 23:12:00 +0200

    send deploy token via query var

--- deploysite.py
+++ deploysite.py
@@ -19,8 +19,8 @@ def deploy():
 
     UPDATE_ACCESS_TOKEN = os.getenv("UPDATE_ACCESS_TOKEN_SITE")
 
-    url = f"https://pyzo.org/site-update/{UPDATE_ACCESS_TOKEN}"
-    # url = f'http://localhost/site-update/{UPDATE_ACCESS_TOKEN}?domain=xx.pyzo.org'
+    url = f"https://pyzo.org/site-update/?token={UPDATE_ACCESS_TOKEN}"
+    # url = f'http://localhost/site-update/?token={UPDATE_ACCESS_TOKEN}&domain=xx.pyzo.org'
     r = requests.post(url, data=f.getvalue())
     if r.status_code != 200:
         raise RuntimeError("Publish failed: " + r.text)
